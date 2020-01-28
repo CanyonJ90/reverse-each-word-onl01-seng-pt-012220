737d12c7c169e03920e39c233f682666e66db2c4
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-28 19:28:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   first_array = sentence1.split("")
-  newarray=[]
-  sentence1.each do |sentence1|
+  new_array=[]
+  first_array.each do |sentence1|
     new_array << sentence1.reverse
   end
   new_array.join("")
