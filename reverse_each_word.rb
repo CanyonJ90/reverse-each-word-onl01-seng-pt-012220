e0ebd8fd94b1f0c696924c29f2d8890a63470d71
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-28 19:23:34 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,27 @@
+def reverse_each_word(sentence1)
+  first_array = sentence1.split("")
+  newarray=[]
+  sentence1.each do |sentence1|
+    new_array << sentence1.reverse
+  end
+  new_array.join("")
+end
+
+
+def reverse_each_word(sentence1)
+  first_array = sentence1.split("")
+  newarray=[]
+  sentence1.collect do |sentence1|
+    new_array << sentence1.reverse
+  end
+  new_array.join("")
+end
+
+
+
+
+
+
+
+
+reverse_each_word("Hello there, and how are you?")
